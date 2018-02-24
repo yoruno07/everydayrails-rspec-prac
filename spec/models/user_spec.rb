a71dbc013441fdf19aa08f9cf9b5f57fd6d0eb16
yoruno07 <ryo.yoru.io@gmail.com>
2018-02-24 15:23:13 +0900

    プロジェクトのバリデーションテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,22 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to include("can't be blank")
   end
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid without a duplicate email address"
+  it "is invalid without a duplicate email address" do
+    User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+      )
+    user = User.new(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+      )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string"
 end
