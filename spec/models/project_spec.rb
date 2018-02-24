b4fc5d27d891182b72b56a7b8269ed6b7198ffe1
yoruno07 <ryo.yoru.io@gmail.com>
2018-02-24 17:04:30 +0900

    projectテストをリファクタリング

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,51 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
-  it "does not allow duplicate project names per user" do
-    user = User.create(
+
+  before do
+    @user = User.create(
       first_name: "Joe",
       last_name: "Tester",
       email: "joetester@examplae.com",
       password: "dottle-nouveau-pavilion-tights-furze",
       )
-
-    user.projects.create(
-      name: "Test Project"
-      )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-      )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
   end
 
-  # 二人のユーザーが同じ名前を使うことは許可すること
-  it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@examplae.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    user.projects.create(
+  # ユーザー、プロジェクト名があれば有効な状態であること
+  it "is valid with a user, name" do
+    project = Project.new(
       name: "Test Project",
+      owner: @user,
       )
+    expect(project).to be_valid
+  end
 
-    other_user = User.create(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "janetester@examplae.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-      )
+  # プロジェクト名がなければ無効な状態であること
+  it "is invalid without a name" do
+    project = Project.new(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+  end
 
-    other_project = other_user.projects.build(
-      name: "Test Project",
-      )
+  # プロジェクト名の重複テスト
+  describe "project name test when user create " do
+    # 同ユーザーで重複する時
+    context "same user create same name of project" do
+    # ユーザー単位では重複したプロジェクト名を許可しないこと
+      it "does not allow duplicate project names per user" do
+        @user.projects.create(
+          name: "Test Project"
+          )
+        new_project = @user.projects.build(
+          name: "Test Project",
+          )
+        new_project.valid?
+        expect(new_project.errors[:name]).to include("has already been taken")
+      end
+    end
 
-    expect(other_project).to be_valid
+    # 別ユーザーで重複する時
+    context "other user create same name of project" do
+      # 二人のユーザーが同じ名前を使うことは許可すること
+      it "allows two users to share a project name" do
+        @user.projects.create(
+          name: "Test Project",
+          )
+        other_user = User.create(
+          first_name: "Jane",
+          last_name: "Tester",
+          email: "janetester@examplae.com",
+          password: "dottle-nouveau-pavilion-tights-furze",
+          )
+        other_project = other_user.projects.build(
+          name: "Test Project",
+          )
+        expect(other_project).to be_valid
+      end
+    end
   end
 end
