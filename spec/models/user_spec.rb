e10fedd24350d0236eadefe63cdfba1768dccb09
yoruno07 <ryo.yoru.io@gmail.com>
2018-02-24 15:59:23 +0900

    noteモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,5 +47,12 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to include("has already been taken")
   end
   # ユーザーのフルネームを文字列として返すこと
-  it "returns a user's full name as a string"
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@example.com",
+      )
+    expect(user.name).to eq "John Doe"
+  end
 end
