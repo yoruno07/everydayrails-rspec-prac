d8f300996a1175132832bbd3f95bcabeb923c173
yoruno07 <ryo.yoru.io@gmail.com>
2018-03-02 00:58:43 +0900

    project_specのユーザー作成部分をFactoryを使用するように変更

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
 
   before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@examplae.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-      )
+    @user = FactoryBot.create(:user)
   end
 
   # ユーザー、プロジェクト名があれば有効な状態であること
@@ -57,12 +52,7 @@ RSpec.describe Project, type: :model do
         @user.projects.create(
           name: "Test Project",
           )
-        other_user = User.create(
-          first_name: "Jane",
-          last_name: "Tester",
-          email: "janetester@examplae.com",
-          password: "dottle-nouveau-pavilion-tights-furze",
-          )
+        other_user = FactoryBot.create(:user)
         other_project = other_user.projects.build(
           name: "Test Project",
           )
