3979b0e3937883de3a901be44cdb74a0d5ddadb8
yoruno07 <ryo.yoru.io@gmail.com>
2018-03-04 17:25:07 +0900

    レスポンス確認テストを追加

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-
+  describe "#index" do
+    it "respond successfully" do
+      get :index
+      expect(response).to be_success
+    end
+  end
 end
