b224d1386fa62b2ea4126c0d4a3a89391efabd4d
yoruno07 <ryo.yoru.io@gmail.com>
2018-03-04 19:39:29 +0900

    deleteアクションのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -171,4 +171,72 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe "#delete" do
+    # 認可されたユーザーとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # プロジェクトを削除できること
+      it "delete a project" do
+        sign_in @user
+        expect {
+          delete :destroy, params: {id: @project.id}
+        }.to change(@user.projects, :count).by(-1)
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # プロジェクトを削除できないこと
+      it "does not delete the project" do
+        sign_in @user
+        expect {
+          delete :destroy, params: {id: @project.id}
+        }.to_not change(Project, :count)
+      end
+
+      # ダッシュボードにリダイレクトすること
+      it "redirets to the dashboard" do
+        sign_in @user
+        delete :destroy, params: {id: @project.id}
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      before do
+        @project = FactoryBot.create(:project)
+      end
+
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        delete :destroy, params: {id: @project.id}
+        expect(response).to have_http_status "302"
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirets to the sign-in page" do
+        delete :destroy, params: {id: @project.id}
+        expect(response).to redirect_to "/users/sign_in"
+      end
+
+      # プロジェクトを削除できないこと
+      it "does not delete the project" do
+        expect {
+          delete :destroy, params: {id: @project.id}
+        }.to_not change(Project, :count)
+      end
+    end
+  end
 end
