f8fb2845f2e9349f251c370838918fb2d6e1f35e
yoruno07 <ryo.yoru.io@gmail.com>
2018-03-05 23:04:37 +0900

    create時のエラーテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -78,13 +78,28 @@ RSpec.describe ProjectsController, type: :controller do
         @user = FactoryBot.create(:user)
       end
 
-      # プロジェクトを追加できること
-      it "adds a project" do
-        project_params = FactoryBot.attributes_for(:project)
-        sign_in @user
-        expect {
-          post :create, params: {project: project_params}
-        }.to change(@user.projects, :count).by(1)
+      # 有効な属性値の場合
+      context "with valid attributes" do
+        # プロジェクトを追加できること
+        it "adds a project" do
+          project_params = FactoryBot.attributes_for(:project)
+          sign_in @user
+          expect {
+            post :create, params: {project: project_params}
+          }.to change(@user.projects, :count).by(1)
+        end
+      end
+
+      # 無効な属性値の場合
+      context "with invalid attributes" do
+        # プロジェクトを追加できないこと
+        it "does not add ad project" do
+          project_params = FactoryBot.attributes_for(:project, :invalid)
+          sign_in @user
+          expect {
+            post :create, params: {project: project_params}
+          }.to_not change(@user.projects, :count)
+        end
       end
     end
 
