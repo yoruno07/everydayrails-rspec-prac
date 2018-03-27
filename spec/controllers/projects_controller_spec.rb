63a2c4f5d68236232627351fea3108fe3c53f6e0
yoruno07 <ryo.yoru.io@gmail.com>
2018-03-27 22:45:42 +0900

    失敗の集約化とヘルパーメソッドへの切り出し

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -12,14 +12,10 @@ RSpec.describe ProjectsController, type: :controller do
       it "response successfully" do
         sign_in @user
         get :index
-        expect(response).to be_success
-      end
-
-      # 200レスポンスを返すこと
-      it "return a 200 response" do
-        sign_in @user
-        get :index
-        expect(response).to have_http_status "200"
+        aggregate_failures do
+          expect(response).to be_success
+          expect(response).to have_http_status "200"
+        end
       end
     end
 
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -15,8 +15,10 @@ RSpec.feature "Projects", type: :feature do
 
     }.to change(user.projects, :count).by(1)
 
-    expect(page).to have_content "Project was successfully created"
-    expect(page).to have_content "Test Project"
-    expect(page).to have_content "Owner: #{user.name}"
+    aggregate_failures do
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to have_content "Owner: #{user.name}"
+    end
   end
 end
