f74110e669a41f07d066729cdb411437abc0c934
yoruno07 <ryo.yoru.io@gmail.com>
2018-05-12 19:33:42 +0900

    testのmailerのHost指定を追加

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   Paperclip::Attachment.default_options[:path] = \
     "#{Rails.root}/spec/test_uploads/:class/:id_partition/:style.:extention"
 end
