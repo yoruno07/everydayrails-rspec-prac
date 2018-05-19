c4a585939e44a370aca5952c4a7b525da523a265
yoruno07 <ryo.yoru.io@gmail.com>
2018-05-19 15:39:49 +0900

    プロジェクトを完了済みにする機能を追加

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -53,17 +53,18 @@ RSpec.feature "Projects", type: :feature do
   end
 
   # ユーザーはプロジェクトを完了済みにする
-  scenario "user completes a prohect", :focus do
+  scenario "user completes a project" do
     user = FactoryBot.create(:user)
     project = FactoryBot.create(:project, owner: user)
     login_as user, scope: :user
 
     visit project_path(project)
+    expect(page).to_not have_content "Completed"
     click_button "Complete"
 
     expect(project.reload.completed?).to be true
     expect(page).to \
-      have_content "Congratilation, this project is complete!"
+      have_content "Congratulations, this project is complete!"
     expect(page).to have_content "Completed"
     expect(page).to_not have_button "Complete"
   end
